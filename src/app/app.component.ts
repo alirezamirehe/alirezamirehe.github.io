23676c258707484c54fe48442ce94b02cd92a43b
Seyed Alireza Mirehe <alirezamirehe@gmail.com>
2024-05-19 01:44:38 +0330

    * Added: Added some simple code
    Signed-off-by: Alireza <alirezamirehe@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('headerAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('500ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
